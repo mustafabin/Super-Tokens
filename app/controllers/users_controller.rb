3df0f1901c37d34ac914c7ac32c5206e4b8a3459
Mustafa Binalhag <mbin828@gmail.com>
2022-11-22 15:38:00 -0500

    moved auth methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,3 @@
 class UsersController < ApplicationController
-    before_action :authorize, only: [:profile]
-    def create
-        # create user from params 
-        user = User.create!(email: params[:email], password: params[:password])
-        # generate token with custom method
-        token = SuperToken.generate_token(user, request)
-        render json: user, serializer: UserTokenSerializer 
-    end
-    def profile 
-        render json: @user
-    end
-    def login 
-        user = User.find_by!(email:params[:email]).try(:authenticate, params[:password])
-        if user
-            # generate token with custom method
-            token = SuperToken.generate_token(user, request)
-            render json: user, serializer: UserTokenSerializer 
-        else
-            render json: {message: "Incorrect password"}
-        end
-    end
+
 end
